4739c1d9a50f3a55d2b686463807cc87c68f4dc3
Bono IJpelaar <ijpelaar_9@hotmail.com>
2019-03-18 20:38:07 +0100

    Added some more test apps and added a status code resolver

--- src/app/dashboard/application/application.component.ts
+++ src/app/dashboard/application/application.component.ts
@@ -11,10 +11,33 @@ export class ApplicationComponent implements OnInit {
   @Input() appName: string;
   @Input() url: string;
 
+  statusMessage: string;
+  statusClass: string;
+
   constructor(private appService: ApplicationService) { }
 
   ngOnInit() {
-    this.appService.checkStatus(this.url);
+    // Has to be a subscription when API is available
+    this.resolveStatus(this.appService.checkStatus(this.url));
+  }
+
+  resolveStatus(status: number) {
+    if (status < 199) {
+      this.statusMessage = `Informational: ${status}`;
+      this.statusClass = 'informational';
+    } else if (status < 299) {
+      this.statusMessage = `Success: ${status}`;
+      this.statusClass = 'success';
+    } else if (status < 399) {
+      this.statusMessage = `Redirection: ${status}`;
+      this.statusClass = 'redirection';
+    } else if (status < 499) {
+      this.statusMessage = `Client Error: ${status}`;
+      this.statusClass = 'client-error';
+    } else {
+      this.statusMessage = `Server Error: ${status}`;
+      this.statusClass = 'server-error';
+    }
   }
 
 }
